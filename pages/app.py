7853f8465ae4b705466fd703f820e424ac2e0879
nongkarn2003 <158234555+nongkarn2003@users.noreply.github.com>
2024-05-09 22:28:11 +0700

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -64,7 +64,7 @@ st.subheader('Forecast data')
 st.write(forecast.tail())
 
 fig1 = plot_plotly(m, forecast)
-components.html(fig1.to_html(full_html=False), height=700,use_container_width=True )
+components.html(fig1.to_html(full_html=True), height=700)
 
 fig2 = m.plot_components(forecast)
 st.pyplot(fig2)
