75e861422f177a983777d8976bb839d785e1918a
nongkarn2003 <158234555+nongkarn2003@users.noreply.github.com>
2024-05-09 17:19:09 +0700

    Update app.py

--- pages/app.py
+++ pages/app.py
@@ -4,6 +4,9 @@ import yfinance as yf
 from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objects as go
+import plotly.graph_objs as go
+
+
 
 
 # เปลี่ยนแปลงพอร์ตเซิร์ฟเวอร์เมื่อมีการรัน Streamlit app
