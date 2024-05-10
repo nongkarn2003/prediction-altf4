867accc5a1e6b84c65e99e43eac1cd6ce2a78a38
nongkarn2003 <158234555+nongkarn2003@users.noreply.github.com>
2024-05-10 15:48:36 +0700

    Update พยากรณ์แนวโน้มหุ้น.py

--- pages/พยากรณ์แนวโน้มหุ้น.py
+++ pages/พยากรณ์แนวโน้มหุ้น.py
@@ -45,8 +45,8 @@ data_load_state.text("Loading data...done")
 
 def plot_raw_data():
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'],name='stock_open'))
-    fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'],name='stock_close'))
+    fig.add_trace(go.Scatter(x=data['Date'],y=data['Open'],name='ราคาเปิด'))
+    fig.add_trace(go.Scatter(x=data['Date'],y=data['Close'],name='ราคาปิด'))
     fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
     st.plotly_chart(fig)
 
